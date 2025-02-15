096445446de457c11398ff669db69c23faf96404
Kavin Abregana <abreganakavin@gmail.com>
2025-02-15 23:53:18 +0800

    adding routes, assets

--- _next-js-review/PropertiesPulse/property-pulse-nextjs/app/layout.jsx
+++ _next-js-review/PropertiesPulse/property-pulse-nextjs/app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "real estate, property, rental property, property management",
+  description: "Find your next rental property with Property Pulse",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
